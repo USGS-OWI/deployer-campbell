6b41501490e2890c7e328675d75a006e9121e962
Luke Winslow <lwinslow@usgs.gov>
2014-06-17 15:03:11 -0500

    Created working Maven plugin for pushing programs to loggernet.

--- src/main/java/gov/usgs/cida/deployer/campbell/MyMojo.java
+++ src/main/java/gov/usgs/cida/deployer/campbell/MyMojo.java
@@ -16,66 +16,97 @@ package gov.usgs.cida.deployer.campbell;
  * limitations under the License.
  */
 
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 
 /**
  * Goal which touches a timestamp file.
  *
- * @goal touch
- * 
- * @phase process-sources
+ * @deprecated Don't use!
  */
+@Mojo( name = "deploy", defaultPhase = LifecyclePhase.DEPLOY )
 public class MyMojo
     extends AbstractMojo
 {
     /**
      * Location of the file.
-     * @parameter expression="${project.build.directory}"
-     * @required
      */
-    private File outputDirectory;
-
+    @Parameter( property="site", required=true )
+    private String _site;
+    
+    @Parameter(property="program_file", required=true)
+    private File _program_file;
+    
+    @Parameter( property="campbell.cora_path", defaultValue="C:/Program Files (x86)/Campbellsci/LoggerNet/cora_cmd.exe", required=true)
+    private File _cora_path;
+    
+    @Parameter(property="campbell.loggernet_url", defaultValue="localhost", required=true)
+    private String _loggernet_url;
+    
     public void execute()
         throws MojoExecutionException
     {
-        File f = outputDirectory;
+        getLog().info("Starting deployment");
+        
+        ProcessBuilder pb = new ProcessBuilder();
+        pb.command(_cora_path.getAbsolutePath());
+        
+        try{
+            Process cora = pb.start();
 
-        if ( !f.exists() )
-        {
-            f.mkdirs();
-        }
+            InputStream input   = cora.getInputStream();
+            OutputStream output = cora.getOutputStream();
 
-        File touch = new File( f, "touch.txt" );
+            BufferedReader buffin = new BufferedReader(new InputStreamReader(input));
+            BufferedWriter buffout = new BufferedWriter(new OutputStreamWriter(output));
 
-        FileWriter w = null;
-        try
-        {
-            w = new FileWriter( touch );
-
-            w.write( "touch.txt" );
-        }
-        catch ( IOException e )
-        {
-            throw new MojoExecutionException( "Error creating file " + touch, e );
-        }
-        finally
-        {
-            if ( w != null )
-            {
-                try
-                {
-                    w.close();
-                }
-                catch ( IOException e )
-                {
-                    // ignore
-                }
+            while(buffin.ready()){
+                String tmp = buffin.readLine();
+                getLog().debug(tmp);
             }
+            
+            getLog().info("Connecting to loggnet at:"+_loggernet_url);
+            buffout.write("connect "+_loggernet_url+";");
+            buffout.flush();
+            Thread.sleep(2000);
+            while(buffin.ready()){
+                String tmp = buffin.readLine();
+                getLog().info(tmp);
+            }
+            
+            getLog().info("Sending program file...");
+            getLog().info("Site:"+_site);
+            getLog().info("File:"+_program_file.getAbsolutePath());
+            buffout.write("send-program-file "+_site+" \""+_program_file.getAbsolutePath()+"\";");
+            buffout.flush();
+            Thread.sleep(10000);
+            while(buffin.ready()){
+                String tmp = buffin.readLine();
+                getLog().info(tmp);
+            }
+            
+            buffout.write("exit;");
+            buffout.flush();
+            Thread.sleep(2000);
+            
+        }catch(IOException ioe){
+            getLog().error(ioe);
+        }catch(InterruptedException ie){
+            getLog().error(ie);
         }
+        
     }
 }
